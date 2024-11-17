0e804ee71f0fb95aed09b8bde9d72a46383e59bb
lcchaverrahelpp <lcchaverra@helppeoplecloud.com>
2024-11-16 23:46:33 -0500

    refactor: se agregan variables de entorno para las conexiones a las bases de datos y middlewares para autenticacion

--- models/Reserva_SQL.js
+++ models/Reserva_SQL.js
@@ -1,6 +1,8 @@
+const {mysql_database} = require('../config')
+
 const { Sequelize, DataTypes } = require('sequelize');
-const sequelize = new Sequelize('chiritour', 'root', '', {
-  host: 'localhost',
+const sequelize = new Sequelize(`${mysql_database.db}`, `${mysql_database.user}`, `${mysql_database.pass}`, {
+  host: `${mysql_database.host}`,
   dialect: 'mysql',
 });
 
